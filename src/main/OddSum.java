f7937a01039bf1b12c6fb54df8f63615142e952c
peggy.nikolaus <peggy.nikolaus@telekom.de>
2022-02-25 14:25:29 +0100

    Quest Commit

--- src/main/OddSum.java
+++ src/main/OddSum.java
@@ -10,15 +10,21 @@ public class OddSum {
 
         ArrayList<Integer> listInt = new ArrayList<>();
         int res;
+        try {
         if (!test) {
             System.out.println("Elements' list :");
             for (String v : args)
                 System.out.print(" " + v);
             System.out.println();
         }
+        } catch (Exception e){
+        	System.err.println("ups");
+        }
+        try {
         for (int i = 0; i < args.length; i++) {
             listInt.add(Integer.parseInt(args[i]));
         }
+        
 
         res = sum(listInt);
         if (test) {
@@ -27,17 +33,23 @@ public class OddSum {
 
         System.out.println("Sum of odd elements : " + res);
         System.out.println("END!");
-
+        } catch (Exception e) {
+        	System.err.println("upps");
+        }
         return 0;
     }
 
     public static int sum(ArrayList<Integer> listInt) {
 
         int result = 0;
+        try {
         for (int value : listInt) {
             if (value % 2 != 0) {
                 result += value;
             }
+        }
+        } catch (Exception e) {
+        	System.err.println("uppps");
         }
         return result;
     }
